b8083922ef2bf41faf5d569525bc378b56ed0037
Mercy-Muchai <mercy.muchai@andela.com>
2017-08-14 12:22:44 +0300

    [fix] methods in class

--- gamesapi/settings.py
+++ gamesapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#3es*1%d(cnd-nm18z$u-q^98s!2#tdn6h!8$@qz#hjs@g)sge'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
